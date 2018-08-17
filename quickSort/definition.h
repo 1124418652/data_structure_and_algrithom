1e2af58b9b9bd853b5e74c31c3c4b00478ba97e4
hejun <1124418652@qq.com>
2018-08-17 11:56:45 +0800

    define function quickSort_with_pointExchange

--- quickSort/definition.h
+++ quickSort/definition.h
@@ -30,3 +30,33 @@ void quickSort(int *list, int min, int max)
 		quickSort(list, left+1, max);
 	}
 }
+
+void quickSort_with_pointExchange(int *list, int min, int max)
+{
+	if (min<max)
+	{
+		int pivotkey = list[min];
+		int left = min;
+		int right = max;
+		int tmp;
+		while (left<right)
+		{
+			while (list[right]>pivotkey && left<right)
+			{
+				right--;
+			}
+			while (list[left]<pivotkey && left<right)
+			{
+				left++;
+			}
+			
+			list[left] = list[left]^list[right];
+			list[right] = list[left]^list[right];
+			list[left] = list[right]^list[left];
+			
+		}
+		list[left] = pivotkey;
+		quickSort_with_pointExchange(list, min, left-1);
+		quickSort_with_pointExchange(list, left+1, max);
+	}
+}
--- quickSort/main.cpp
+++ quickSort/main.cpp
@@ -12,4 +12,15 @@ void main()
 		cout<<a[i]<<' ';
 	}
 	cout<<endl;
+
+	int b[] = {2,11,3,6,9,4,5, NULL};
+	quickSort_with_pointExchange(b,0,6);
+	for (i=0; i<7; i++)
+	{
+		cout<<b[i]<<' ';
+	}
+	cout<<endl;
+	int d = 2, c=2;
+	int e = c^d^c;
+	cout<<e<<endl;
 }
